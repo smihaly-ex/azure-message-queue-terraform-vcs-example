eee5eb107b56bb20fc30d13aafae82ee35c08b77
smihaly-x <sarolta.mihaly@explorer-consulting.ro>
2025-02-12 13:41:27 +0200

    Added comment to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# This is a simple example of how to use the count meta-argument to create multiple instances of a resource.
 resource "null_resource" "test" {
     count = 2
     provisioner "local-exec" {
